1816c1dc2cd74a42372846eae09efbd7778bc911
alexbro <alexbro@umich.edu>
2017-03-11 16:27:13 -0500

    Add support in code for carat operator

--- rpn.py
+++ rpn.py
@@ -10,7 +10,8 @@ OPERATORS = {
             '+' : operator.add,
             '-' : operator.sub,
             '*' : operator.mul,
-            '/' : operator.truediv
+            '/' : operator.truediv,
+            '^' : operator.pow
             }
 
 def calculate(arg):
